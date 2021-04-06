91a93778bbbc922f4c19b44db25d167861282d4e
Marcus <marcus.gamboa@gmail.com>
2021-04-06 17:20:23 -0600

    Added D3. Started adding Bars. Fetched Data.

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-import * as d3 from "d3";
-
-const height = "500px";
-const width = "500px";
+const height = 400;
+const width = 1000;
+const padding = 50;
 const svg = d3
   .select("body")
   .append("svg")
   .attr("height", height)
   .attr("width", width);
 
-console.log(svg);
-console.log("hi");
+fetch(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let barData = data.data;
+    let dummyData = [
+      [0, 300],
+      [0, 400],
+      [0, 500],
+      [0, 600],
+      [0, 700],
+    ];
+    // const xScale = d3
+    //   .scaleLinear()
+    //   .domain([0, d3.max(dummyData, (d) => d[0])])
+    //   .range([padding, width - padding]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dummyData, (d) => d[1])])
+      .range([height - padding, padding]);
+
+    console.log(data);
+    svg
+      .selectAll("rect")
+      .data(dummyData)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * (width / dummyData.length))
+      .attr("y", (d, i) => yScale(d[1]))
+      .attr("width", width / dummyData.length - 1)
+      .attr("height", (d) => yScale(d[1]))
+      .attr("color", "blue");
+  });

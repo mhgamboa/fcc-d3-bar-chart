793575119af76cfc89baeb0962cc354cea381f0b
Marcus <marcus.gamboa@gmail.com>
2021-04-08 16:19:19 -0600

    All tests pass.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fcc-d3-bar-chart
+
+I was taught about the `Title` D3 Attribute, but At least the way I was implementing it the tests weren't working out. So I added a `Text` atribute to replicate the tooltip.
+
+Biggest takeaway was the `.on()` specifically `on.(mouseover)`, `on.(mousemove)`, and `on.(mouseout)`.
+
+Smaller takeaway was rotating text. the translations of rotated text kind of threw me through a loop.
--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const svg = d3
   .attr("height", height)
   .attr("width", width);
 
+// const tooltip = svg.append("g").attr("id", "tooltip");
+
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
   .then((response) => response.json())
+  //Once data is received create everything
   .then((data) => {
     let barData = data.data;
     const barWidth = (width - padding - padding) / barData.length;
@@ -22,8 +25,8 @@ fetch(
       .range([height - padding, padding]);
 
     // create xScale which scales time by the year
-    let minDate = new Date(barData[0][0].substr(0, 4));
-    var maxDate = new Date(barData[barData.length - 1][0].substr(0, 4));
+    let minDate = new Date(barData[0][0]);
+    let maxDate = new Date(barData[barData.length - 1][0]);
 
     const xScale = d3
       .scaleTime()
@@ -45,6 +48,13 @@ fetch(
       .call(xAxis)
       .attr("transform", `translate(0,${height - padding})`)
       .attr("id", "x-axis");
+    // yAxisLabel
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -229)
+      .attr("y", 70)
+      .text("Gross Domestic Product");
     // Create & Append Bars
     svg
       .selectAll("rect")
@@ -57,5 +67,39 @@ fetch(
       .attr("height", (d) => height - padding - yScale(d[1]))
       .attr("class", "bar")
       .attr("data-date", (d) => d[0])
-      .attr("data-gdp", (d) => d[1]);
+      .attr("data-gdp", (d) => d[1])
+      // Mouse Events for bars
+      .on("mouseover", (e, i) => {
+        if (e.layerX < 700) {
+          tooltip.setAttribute("x", e.layerX + 5);
+        } else {
+          tooltip.setAttribute("x", e.layerX - 233);
+        }
+        tooltip.setAttribute("y", e.layerY - 20);
+        tooltip.setAttribute("data-date", i[0]);
+        tooltip.textContent = `Date: ${i[0]} GDP:${i[1]}`;
+        tooltip.classList.toggle("hidden");
+      })
+      .on("mousemove", (e, i) => {
+        if (e.layerX < 700) {
+          tooltip.setAttribute("x", e.layerX + 5);
+        } else {
+          tooltip.setAttribute("x", e.layerX - 233);
+        }
+
+        tooltip.setAttribute("y", e.layerY - 20);
+      })
+      .on("mouseout", (e, i) => {
+        tooltip.classList.toggle("hidden");
+      });
+    // Create ToolTip
+    svg
+      .append("text")
+      .attr("id", "tooltip")
+      .attr("width", "45")
+      .attr("height", "30")
+      .attr("x", "100")
+      .attr("y", "100")
+      .attr("class", "hidden")
+      .text("test");
   });
